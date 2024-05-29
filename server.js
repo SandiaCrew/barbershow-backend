3e82eed1cd681caf7c36fea7edf6c8213fcff81f
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 13:06:37 +0200

    Integrate clients route into server

--- server.js
+++ server.js
@@ -14,14 +14,17 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch(err => {
-    console.error('Error connecting to MongoDB:', err.message);
-  });
-  
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
+// Use the clients route
+const clientsRouter = require('./routes/clients');
+app.use('/clients', clientsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
