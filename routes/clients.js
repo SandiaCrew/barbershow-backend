a4f35dd02bcbc02354da705dc649c12c9a4aeb6e
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 13:52:02 +0200

    Integrate client controller with routes

--- routes/clients.js
+++ routes/clients.js
@@ -1,83 +1,21 @@
 // routes/clients.js
 const express = require('express');
 const router = express.Router();
-const Client = require('../models/Client');
+const clientController = require('../controllers/clientController');
 
 // Get all clients
-router.get('/', async (req, res) => {
-  try {
-    const clients = await Client.find();
-    res.json(clients);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get('/', clientController.getAllClients);
 
 // Get one client
-router.get('/:id', getClient, (req, res) => {
-  res.json(res.client);
-});
+router.get('/:id', clientController.getClient, clientController.getClientById);
 
 // Create a client
-router.post('/', async (req, res) => {
-  const client = new Client({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    visits: req.body.visits,
-  });
-  try {
-    const newClient = await client.save();
-    res.status(201).json(newClient);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.post('/', clientController.createClient);
 
 // Update a client
-router.patch('/:id', getClient, async (req, res) => {
-  if (req.body.name != null) {
-    res.client.name = req.body.name;
-  }
-  if (req.body.email != null) {
-    res.client.email = req.body.email;
-  }
-  if (req.body.phone != null) {
-    res.client.phone = req.body.phone;
-  }
-  if (req.body.visits != null) {
-    res.client.visits = req.body.visits;
-  }
-  try {
-    const updatedClient = await res.client.save();
-    res.json(updatedClient);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.patch('/:id', clientController.getClient, clientController.updateClient);
 
 // Delete a client
-router.delete('/:id', getClient, async (req, res) => {
-  try {
-    await Client.deleteOne({ _id: res.client._id });
-    res.json({ message: 'Deleted Client' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-async function getClient(req, res, next) {
-  let client;
-  try {
-    client = await Client.findById(req.params.id);
-    if (client == null) {
-      return res.status(404).json({ message: 'Cannot find client' });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-  res.client = client;
-  next();
-}
+router.delete('/:id', clientController.getClient, clientController.deleteClient);
 
 module.exports = router;
