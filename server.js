6c600479a311c2fddaf1b3c78b5a2676dd3e37f2
Sandia <sandiacreativecrew@gmail.com>
2024-05-29 12:28:41 +0200

    Add MongoDB connection to server

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+  
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
